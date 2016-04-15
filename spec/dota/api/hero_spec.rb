3410dfe1a9c42083cf578e36608a838c9edaf330
Shaun Dowling <shaun.dowling@gmail.com>
2016-04-15 19:33:50 +0100

    Add 6.86 Hero

--- spec/dota/api/hero_spec.rb
+++ spec/dota/api/hero_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Dota::API::Hero do
   specify ".all" do
     heroes = described_class.all
     expect(heroes.first).to be_a described_class
-    expect(heroes.count).to eq 110
+    expect(heroes.count).to eq 111
   end
 
   specify ".find" do
