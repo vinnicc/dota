daa9c1111eb7eb4edc5e48ec5675a40cb7e6f19f
y4ashida <y4ashida@gmail.com>
2016-04-28 05:00:18 +0900

    Update items count

--- spec/dota/api/item_spec.rb
+++ spec/dota/api/item_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Dota::API::Item do
   specify ".all" do
     items = described_class.all
     expect(items.first).to be_a described_class
-    expect(items.count).to eq 261
+    expect(items.count).to eq 275
   end
 
   specify "#id" do
